30b63446cc5848bede398f646a1b5aaf29f15ab4
Paul Smith <paulsmith@gmail.com>
2024-12-05 16:58:06 -0600

    Day 5, part 2

--- 05/solution.zig
+++ 05/solution.zig
@@ -3,7 +3,7 @@ const input = @embedFile("input.txt");
 
 pub fn main() !void {
     try part1();
-    //try part2();
+    try part2();
 }
 
 const List = std.ArrayList(u64);
@@ -44,6 +44,54 @@ fn part1() !void {
     }
 
     std.debug.print("{}\n", .{total});
+}
+
+fn part2() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer if (gpa.deinit() == .leak) @panic("memory leak");
+
+    var ordering = std.AutoHashMap(u64, List).init(allocator);
+    defer deinitMap(&ordering);
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.peek()) |line| {
+        if (!std.mem.containsAtLeast(u8, line, 1, "|")) {
+            break;
+        }
+        const prec = try std.fmt.parseInt(u64, line[0..2], 10);
+        const page = try std.fmt.parseInt(u64, line[3..], 10);
+        const entry = try ordering.getOrPut(prec);
+        if (!entry.found_existing) entry.value_ptr.* = List.init(allocator);
+        try entry.value_ptr.*.append(page);
+        _ = lines.next();
+    }
+
+    var total: u64 = 0;
+
+    while (lines.next()) |line| {
+        var update = List.init(allocator);
+        defer update.deinit();
+
+        var pages = std.mem.tokenizeScalar(u8, line, ',');
+        while (pages.next()) |page| {
+            try update.append(try std.fmt.parseInt(u64, page, 10));
+        }
+
+        if (!correct(ordering, update.items)) {
+            std.sort.insertion(usize, update.items, ordering, customSort);
+            total += update.items[update.items.len / 2];
+        }
+    }
+
+    std.debug.print("{}\n", .{total});
+}
+
+fn customSort(ordering: std.AutoHashMap(u64, List), lhs: u64, rhs: u64) bool {
+    if (ordering.get(lhs)) |list| {
+        return std.mem.containsAtLeast(u64, list.items, 1, &[_]u64{rhs});
+    }
+    return false;
 }
 
 fn correct(ordering: std.AutoHashMap(u64, List), update: []const u64) bool {
